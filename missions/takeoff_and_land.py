eebda462cfe1eb826036d9a88cd98db4d159bbc4
Wilson talakayala <heywilsont@gmail.com>
2026-05-09 09:33:26 +0530

    Add MAVSDK takeoff and landing mission

---
+++ missions/takeoff_and_land.py
@@ -0,0 +1,26 @@
+import asyncio
+from mavsdk import System
+
+async def run():
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone connection...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone connected")
+            break
+
+    print("Arming...")
+    await drone.action.arm()
+
+    print("Taking off...")
+    await drone.action.takeoff()
+
+    await asyncio.sleep(10)
+
+    print("Landing...")
+    await drone.action.land()
+
+if __name__ == "__main__":
+    asyncio.run(run())
